12857c12f92bd9659cd3cdc371e005eeadc28b21
mmgaw113 <89485705+mmgaw113@users.noreply.github.com>
2022-04-30 16:11:06 -0400

    File Writing

---
+++ Plugins/fileman-master/Source/FileMan/FileMan.Build.cs
@@ -0,0 +1,46 @@
+using UnrealBuildTool;
+
+public class FileMan : ModuleRules
+{
+	public FileMan(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+			}
+			);
+	}
+}
